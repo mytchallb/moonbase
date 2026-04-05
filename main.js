24c0d21dcdc6afba870edfdde01be618e5d6d2f1
Mytch <mytchall.bransgrove@hairylemon.co.nz>
2026-04-05 15:02:06 +1200

    added js with sample data

---
+++ main.js
@@ -0,0 +1,19 @@
+
+const data = {
+  name: "Moon Base Alpha",
+  stats: [
+    {
+      temp: 24,
+      oxygen: 100
+    }
+  ]
+}
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+
+});
